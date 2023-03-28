bf97916c8cf65b85981e56b56c94d8ede795825d
lyubov <lyubanechitaylova@gmail.com>
2023-03-28 17:35:56 +0300

    test lesson 3 methods realiztion

--- Lesson3.java
+++ Lesson3.java
@@ -1,7 +1,19 @@
 import java.util.Random;
 
-public class Lesson3 {
+public class Lesson3
+{
     public static void main(String[] args) {
+        int[] array = generateValues();
+        int sum = sumValues(array);
+
+        // count the sum of numbers greater than 5 and less than 45
+        System.out.println("The sum of numbers greater than 5 and less than 45 = " + sum + "\n");
+        System.out.print("Even positive numbers of the array: ");
+        printEvenNumbers(array);
+    }
+
+    private static int[] generateValues()
+    {
         int[] array = new int[20];
         Random random = new Random();
 
@@ -10,23 +22,31 @@ public class Lesson3 {
             array[i] = random.nextInt(50);
             System.out.println(array[i] + "");
         }
+
+        return array;
+    }
+
+    private static int sumValues(int[] values)
+    {
         // count the sum
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 5 && array[i] < 45) {
-                sum += array[i];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > 5 && values[i] < 45) {
+                sum += values[i];
             }
         }
-        // count the sum of numbers greater than 5 and less than 45
-        System.out.println("The sum of numbers greater than 5 and less than 45 = " + sum + "\n");
-        System.out.print("Even positive numbers of the array: ");
+
+        return sum;
+    }
+
+    private static void printEvenNumbers(int[] array)
+    {
         for (int i = 0; i < array.length; i++) {
             if (array[i] != 0 && array[i] % 2 == 0 ) {
                 System.out.print(array[i] + " ");
             }
         }
     }
-
 }
 
 
