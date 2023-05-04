141a5817515412f9ed95b11882cbcfbc0ebeb9c7
lyubov <lyubanechitaylova@gmail.com>
2023-05-04 19:41:27 +0300

    hometask11 done

--- HomeTask11/PrintMap.java
+++ HomeTask11/PrintMap.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class PrintMap {
     public static void printMap(Map<Integer, String> map) {
         TreeMap<Integer, String> sortedMap = new TreeMap<>(map);
+
         for (Map.Entry<Integer, String> entry : sortedMap.entrySet()) {
             System.out.println(entry.getKey() + "=" + entry.getValue());
         }
