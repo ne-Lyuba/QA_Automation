f21fed1b3de875556622fb5c329f2c31e3614617
lyubov <lyubanechitaylova@gmail.com>
2023-05-04 19:40:31 +0300

    hometask11 done

--- HomeTask11/Main.java
+++ HomeTask11/Main.java
@@ -6,13 +6,14 @@ import static HomeTask11.PrintMap.printMap;
 
 public class Main {
     public static void main(String[] args) {
-        TreeMap<Integer, String> pets = new TreeMap<>();
+        Map<Integer, String> pets = new TreeMap<>();
         pets.put(10000, "Cat");
         pets.put(3234, "Parrot");
         pets.put(2, "Dog");
         pets.put(721, "Cow");
         pets.put(10, "Duck");
         pets.put(59, "Bird");
+
         printMap(pets);
 
     }
